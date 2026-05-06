d5aec6ad7800dc6e3638d707711f26f5be4e1d27
Nico Duvekot <nduvekot7@gmail.com>
2026-05-06 14:25:28 -0700

    Create TestSelectableUnit
    script for a selectable unit

---
+++ Assets/Scripts/Core/Selection/TestSelectableUnit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Selection
+{
+    public class TestSelectableUnit : MonoBehaviour, ISelectable
+    {
+        public MonoBehaviour Behaviour => this;
+
+        public void OnHoverEnter() { Debug.Log($"{name} unit is being hovered over"); }
+        public void OnHoverExit() { Debug.Log($"{name} unit is not longer being hovered over"); }
+        public void OnSelected() { Debug.Log($"{name} unit is selected operation"); }
+        public void OnDeselected() { Debug.Log($"{name} unit is no longer selected"); }
+        public void OnCommand(Vector3 worldPos) { Debug.Log($"{name} unit was given a command click for {worldPos}"); }
+    }
+}
