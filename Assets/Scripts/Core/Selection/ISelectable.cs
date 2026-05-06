ddb4bbb74d55491cd2cae8e11527f4c0f21fdd06
Nico Duvekot <nduvekot7@gmail.com>
2026-05-05 21:57:37 -0700

    Create ISelectable
    interface for any selectable object to have.

---
+++ Assets/Scripts/Core/Selection/ISelectable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Selection
+{
+    public interface ISelectable
+    {
+        // implied that this interface belongs on only Monobehaviours
+        MonoBehaviour Behaviour { get; }
+        
+        public void OnHoverEnter() { }
+        public void OnHoverExit() { }
+        public void OnSelected() { }
+        public void OnDeselected() { }
+        public void OnCommand(Vector3 worldPos) { }
+    }
+}
