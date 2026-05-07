9dec55acc6a5a2e25797e88e5837ea3abea80ff2
Nico Duvekot <nduvekot7@gmail.com>
2026-05-07 16:36:29 -0700

    Create BuilderUI Script
    script for UI for the builder unit

---
+++ Assets/Scripts/Core/UIElements/BuilderUI.cs
@@ -0,0 +1,102 @@
+﻿using Selection;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DataDefinitions;
+
+namespace Core.UIElements
+{
+    public class BuilderUI : MonoBehaviour
+    {
+        public static BuilderUI Instance { get; private set; }
+        
+        [SerializeField] private GameObject panel;
+        
+        [Header("Header Label")]
+        [SerializeField] private TextMeshProUGUI headerLabel;
+        
+        [Header("Buttons")]
+        [SerializeField] private Button buildFarmButton;
+        [SerializeField] private TextMeshProUGUI buildFarmLabel;
+
+        [SerializeField] private Button buildSawmillButton;
+        [SerializeField] private TextMeshProUGUI buildSawmillLabel;
+        
+        [Header("Demolish")]
+        [SerializeField] private Button demolishButton;
+
+        [Header("Building Data Options")]
+        [SerializeField] private BuildingData farmData;
+        [SerializeField] private BuildingData sawmillData;
+        
+        private TestSelectableHex _currentHex;
+        private TestSelectableBuilder _currentBuilder;
+        
+        private void Awake()
+        {
+            Debug.Log("Awake is called by Builder UI");
+            Instance = this;
+            
+            //HoverUI.Instance.Show("Hi");
+
+            buildFarmButton.onClick.AddListener(() => Build(farmData));
+            buildSawmillButton.onClick.AddListener(() => Build(sawmillData));
+            demolishButton.onClick.AddListener(Demolish);
+            
+            buildFarmLabel.text = farmData.buildingName;
+            buildSawmillLabel.text = sawmillData.buildingName;
+        }
+        
+        public void ShowOptionsFor(TestSelectableHex hex, TestSelectableBuilder builder)
+        {
+            _currentHex = hex;
+            _currentBuilder = builder;
+
+            panel.SetActive(true);
+
+            if (hex.AttachedBuilding == null)
+            {
+                headerLabel.text = "Buildable Options";
+
+                buildFarmButton.gameObject.SetActive(hex.ResourceType == ResourceType.Grain);
+                buildSawmillButton.gameObject.SetActive(hex.ResourceType == ResourceType.Wood);
+
+                demolishButton.gameObject.SetActive(false);
+            }
+            else
+            {
+                headerLabel.text = $"Current Building:\n{hex.AttachedBuilding.buildingName}";
+
+                buildFarmButton.gameObject.SetActive(false);
+                buildSawmillButton.gameObject.SetActive(false);
+
+                demolishButton.gameObject.SetActive(true);
+            }
+        }
+
+        public void Hide()
+        {
+            panel.SetActive(false);
+            _currentHex = null;
+            _currentBuilder = null;
+        }
+
+        private void Build(BuildingData data)
+        {
+            if (_currentHex == null || _currentBuilder == null)
+                return;
+
+            _currentBuilder.StartBuilding(data, false);
+            Hide();
+        }
+        
+        private void Demolish()
+        {
+            if (_currentHex == null || _currentBuilder == null)
+                return;
+
+            _currentBuilder.StartBuilding(_currentHex.AttachedBuilding, true);
+            Hide();
+        }
+    }
+}
