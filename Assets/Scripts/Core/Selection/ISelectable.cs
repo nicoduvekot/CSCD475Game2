8cb58df9e5126d0d8cf0ce80136a09f0f341eba9
Nico Duvekot <nduvekot7@gmail.com>
2026-05-07 16:33:18 -0700

    Update ISelectable
    On Command now requires reference to the targetSelectable

--- Assets/Scripts/Core/Selection/ISelectable.cs
+++ Assets/Scripts/Core/Selection/ISelectable.cs
@@ -4,13 +4,12 @@ namespace Selection
 {
     public interface ISelectable
     {
-        // implied that this interface belongs on only Monobehaviours
         MonoBehaviour Behaviour { get; }
         
         public void OnHoverEnter() { }
         public void OnHoverExit() { }
         public void OnSelected() { }
         public void OnDeselected() { }
-        public void OnCommand(Vector3 worldPos) { }
+        public void OnCommand(Vector3 worldPos, ISelectable targetSelectable) { }
     }
 }
