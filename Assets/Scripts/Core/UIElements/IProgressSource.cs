06fa06546d08e4d5d9388146d6af9e8810ada129
Nico Duvekot <nduvekot7@gmail.com>
2026-05-07 16:37:26 -0700

    Create: IProgressSource Script
    script for interface for thing that can display progress using world space UI

---
+++ Assets/Scripts/Core/UIElements/IProgressSource.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.UIElements
+{
+    public interface IProgressSource
+    {
+        bool IsInProgress { get; }
+        float Progress01 { get; }
+        string ProgressLabel { get; }
+    }
+}
