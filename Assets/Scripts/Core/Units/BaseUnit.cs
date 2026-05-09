5e44b59634ee81256acfdaea2ef1555112a42a69
Nico Duvekot <nduvekot7@gmail.com>
2026-05-09 10:54:45 -0700

    Create : BaseUnit script
    script for the base of a unit to use
    holds mostly the state transitions a unit can go through and logic gates for what to do

---
+++ Assets/Scripts/Core/Units/BaseUnit.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using Core.UIElements;
+using Selection;
+using UnityEngine;
+using HealthSystem;
+
+namespace Units
+{
+    [RequireComponent(typeof(Health))]
+    [RequireComponent(typeof(UnitStats))]
+    public abstract class BaseUnit : MonoBehaviour, ISelectable, IDisplayStateUI
+    {
+        public MonoBehaviour Behaviour => this;
+
+        protected Health Health { get; private set; }
+        protected UnitStats Stats { get; private set; }
+        protected UnitStateDisplay StateDisplay { get; private set; }
+
+        protected UnitState _state = UnitState.Idle;
+        protected BaseUnit _targetEnemy;
+        
+        protected float _attackCooldownTimer;
+
+        private const float FakeDeathAnimTime = 1.5f;
+
+        protected virtual void Awake()
+        {
+            Health = GetComponent<Health>();
+            Stats = GetComponent<UnitStats>();
+            
+            Health.InitializeHealth(Stats.BaseMaxHealth);
+            Health.OnHealthEmpty += HandleDeath;
+            
+            StateDisplay = GetComponentInChildren<UnitStateDisplay>();
+            if (StateDisplay != null) StateDisplay.Initialize(this, transform);
+        }
+        
+        protected virtual void OnDestroy()
+        {
+            if (Health != null) Health.OnHealthEmpty -= HandleDeath;
+        }
+
+        public string StateLabel => GetStateDisplayText();
+        
+        protected virtual string GetStateDisplayText()
+        {
+            return _state.ToString();
+        }
+        
+        // public API
+        
+        public void TakeDamage(float amount)
+        {
+            Health.ApplyDamage(amount);
+        }
+        
+        public virtual void OnCommand(Vector3 worldPos, ISelectable targetSelectable)
+        {
+            if (targetSelectable is BaseUnit enemy)
+            {
+                _targetEnemy = enemy;
+                SetState(UnitState.Engaging);
+                return;
+            }
+            
+            // handle movement from command here
+        }
+        
+        protected virtual void Update()
+        {
+            switch (_state)
+            {
+                case UnitState.Engaging:
+                    HandleEngaging();
+                    break;
+
+                case UnitState.Engaged:
+                    HandleEngaged();
+                    break;
+                
+                case UnitState.Dying:
+                    
+                    break;
+            }
+        }
+
+        protected virtual void HandleEngaging()
+        {
+            if (_targetEnemy == null)
+            {
+                SetState(UnitState.Idle);
+                return;
+            }
+            
+            // get distance to enemy
+            float distance = Vector3.Distance(transform.position, _targetEnemy.transform.position);
+            
+            // if in range => engage
+            if (distance <= Stats.BaseAttackRange)
+            {
+                SetState(UnitState.Engaged);
+                _attackCooldownTimer = 0f;
+                return;
+            }
+
+            // if out of range => move toward target
+            TryMoveTowards(_targetEnemy.transform.position);
+        }
+
+        protected virtual void HandleEngaged()
+        {
+            if (_targetEnemy == null)
+            {
+                SetState(UnitState.Idle);
+                return;
+            }
+            
+            FaceTarget(_targetEnemy.transform.position);
+            
+            // get distance to enemy
+            float distance = Vector3.Distance(transform.position, _targetEnemy.transform.position);
+            
+            // if unit moved out of range logic
+            if (distance > Stats.BaseAttackRange)
+            {
+                SetState(UnitState.Engaging);
+                return;
+            }
+            
+            // else attack logic
+            _attackCooldownTimer -= Time.deltaTime;
+
+            if (_attackCooldownTimer <= 0f)
+            {
+                _attackCooldownTimer = 1f / Stats.BaseAttackSpeed;
+                Attack(_targetEnemy);
+            }
+        }
+        
+        protected virtual void Attack(BaseUnit enemy)
+        {
+            enemy.TakeDamage(Stats.BaseAttackPower);
+        }
+
+        protected virtual void TryMoveTowards(Vector3 targetPos)
+        {
+            //Debug.LogWarning("Unit is trying to move : This is not implemented though!");
+        }
+        
+        protected void FaceTarget(Vector3 targetPos)
+        {
+            Vector3 dir = targetPos - transform.position;
+            dir.y = 0f;
+
+            if (dir.sqrMagnitude > Mathf.Epsilon)
+                transform.rotation = Quaternion.LookRotation(dir);
+        }
+        
+        private void HandleDeath()
+        {
+            if (_state == UnitState.Dying) return;
+
+            SetState(UnitState.Dying);
+
+            OnDeathAnimationStarted();
+        }
+        
+        protected virtual void OnDeathAnimationStarted()
+        {
+            StartCoroutine(FakeDeathAnimationRoutine());
+        }
+        
+        private IEnumerator FakeDeathAnimationRoutine()
+        {
+            Debug.LogWarning("Base Unit is faking death animation");
+            yield return new WaitForSeconds(FakeDeathAnimTime);
+            OnDeathAnimationCompleted();
+        }
+        
+        protected virtual void OnDeathAnimationCompleted()
+        {
+            Destroy(gameObject);
+        }
+
+        #region State Machine
+
+        protected enum UnitState
+        {
+            Idle,
+            Moving,
+            Engaging,
+            Engaged,
+            Dying
+        }
+
+        protected void SetState(UnitState newState)
+        {
+            _state = newState;
+            // if we do anims, the transitions can happen here?
+        }
+
+        #endregion // state machine
+        
+
+        protected virtual void OnDrawGizmos()
+        {
+            // Draw a cyan line showing the forward direction
+            Gizmos.color = Color.cyan;
+
+            Vector3 start = transform.position + Vector3.up * 0.1f;
+            Vector3 end = start + transform.forward * 1.5f;
+
+            Gizmos.DrawLine(start, end);
+            Gizmos.DrawSphere(end, 0.05f);
+        }
+    }
+}
