3750c697566d0472ff5ffa473549559fa893d9e7
Nico Duvekot <nduvekot7@gmail.com>
2026-05-09 10:55:22 -0700

    Create : BaseUnitEditor script
    script for base unit editor tools

---
+++ Assets/Scripts/Core/Units/Editor/BaseUnitEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Units.Editor
+{
+    [CustomEditor(typeof(BaseUnit), true)]
+    public class BaseUnitEditor : UnityEditor.Editor
+    {
+        private float debugDamage = 10f;
+        
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (Application.isPlaying)
+            {
+                EditorGUILayout.Space(10);
+                EditorGUILayout.LabelField("Debug Tools", EditorStyles.boldLabel);
+
+                debugDamage = EditorGUILayout.FloatField("Damage Amount", debugDamage);
+
+                if (GUILayout.Button("Apply Damage"))
+                {
+                    BaseUnit unit = (BaseUnit)target;
+                    unit.TakeDamage(debugDamage);
+                }
+            }
+        }
+    }
+}
