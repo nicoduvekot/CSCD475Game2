984188bfb00b8d7e51aacf3e50b7b3114b5c0a7e
Nico Duvekot <nduvekot7@gmail.com>
2026-05-22 21:19:33 -0700

    Update UnitAnimator
    cleanup and organization

--- Assets/Scripts/Core/Units/UnitAnimator.cs
+++ Assets/Scripts/Core/Units/UnitAnimator.cs
@@ -8,6 +8,7 @@ namespace Units
         private BaseUnit _unit;
         
         private static readonly int IsAttacking = Animator.StringToHash("IsAttacking");
+        private static readonly int AttackSpeed = Animator.StringToHash("AttackSpeed"); 
         private static readonly int IsWalking = Animator.StringToHash("IsWalking");
         private static readonly int DeathTrigger = Animator.StringToHash("Death");
 
@@ -16,17 +17,37 @@ namespace Units
             _animator = GetComponent<Animator>();
             _unit = GetComponentInParent<BaseUnit>();
         }
-        
-        public void SetAttacking(bool value)
-        {
-            _animator.SetBool(IsAttacking, value);
-        }
+
+        #region Walking Logic Operations
 
         public void SetWalking(bool value)
         {
             _animator.SetBool(IsWalking, value);
         }
 
+        #endregion
+
+        #region Attack Logic Operations
+
+        public void SetAttacking(bool value)
+        {
+            _animator.SetBool(IsAttacking, value);
+        }
+
+        public void SetAttackSpeed(float value)
+        {
+            _animator.SetFloat(AttackSpeed, value);
+        }
+
+        public void OnAttackAnimationHit()
+        {
+            _unit.OnAttackHit();
+        }
+
+        #endregion
+        
+        #region Death Logic Operations
+        
         public void TriggerDeath()
         {
             _animator.SetTrigger(DeathTrigger);
@@ -36,5 +57,7 @@ namespace Units
         {
             _unit.OnDeathAnimationCompleted();
         }
+        
+        #endregion
     }
 }
