ef02f01a8ddc7a91db8a3e40084d1fd46eff919a
Nico Duvekot <nduvekot7@gmail.com>
2026-05-24 20:00:30 -0700

    Create PerspectiveManagerEditor
    editor tools for changing perspective

---
+++ Assets/Scripts/Core/TeamControl/Editor/PerspectiveManagerEditor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TeamControl.Editor
+{
+    [CustomEditor(typeof(PerspectiveManager))]
+    public class PerspectiveManagerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            PerspectiveManager manager = (PerspectiveManager)target;
+
+            EditorGUILayout.Space(10);
+            EditorGUILayout.LabelField("Editor Tools", EditorStyles.boldLabel);
+
+            // Dropdown for perspective
+            Perspective newPerspective = (Perspective)EditorGUILayout.EnumPopup(
+                "Current Perspective",
+                manager.CurrentPerspective
+            );
+
+            // If changed, apply immediately
+            if (newPerspective != manager.CurrentPerspective)
+            {
+                if (Application.isPlaying)
+                {
+                    manager.SetPerspective(newPerspective);
+                }
+                else
+                {
+                    // Edit mode preview
+                    manager.SetPerspective(newPerspective);
+                    EditorUtility.SetDirty(manager);
+                }
+            }
+        }
+    }
+}
