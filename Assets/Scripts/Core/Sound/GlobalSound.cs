3c4e69d926bcb4580e656f129ea964fb3227a8d5
warrenh1913 <warrenh1913@gmail.com>
2026-05-27 12:35:34 -0700

    added basic sound

---
+++ Assets/Scripts/Core/Sound/GlobalSound.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public static class GlobalSound : MonoBehaviour
+{
+    private static AudioSource dynamicAudio = new AudioSource();
+    private static AudioSource music = new AudioSource();
+
+    public static AudioClip soldierFight;
+    public static AudioClip archerFight;
+    public static AudioClip horsesFight;
+    public static AudioClip unitDies;
+    public static AudioClip horseDies;
+
+
+    public static AudioClip movement;
+    public static AudioClip win;
+    public static AudioClip lose;
+    public static AudioClip capturePoint;
+    public static AudioClip losePoint;
+
+
+    
+
+
+    public 
+}
