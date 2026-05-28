b2ddbe3a0867e4e9f4a787c3c0e13e9e2bdf66d4
Nico Duvekot <nduvekot7@gmail.com>
2026-05-28 11:32:45 -0700

    Create Example_RL
    just the main operations for RL agent example

---
+++ Assets/Scripts/Core/RL_Agent/Example_RL.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+namespace RL_Agent
+{
+    public class Example_RL : Agent
+    {
+        public override void Initialize()
+        {
+            
+        }
+
+        // set up episode values
+        public override void OnEpisodeBegin()
+        {
+            
+        }
+
+        // read the state
+        public override void CollectObservations(VectorSensor sensor)
+        {
+            
+        }
+
+        // use this to mask actions
+        public override void WriteDiscreteActionMask(IDiscreteActionMask actionMask)
+        {
+            
+        }
+
+        // user input influence
+        public override void Heuristic(in ActionBuffers actionsOut)
+        {
+            
+        }
+    }
+}
