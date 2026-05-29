77fbf4ddda5c7580cd0069da7bdf4775522dc749
Nico Duvekot <nduvekot7@gmail.com>
2026-05-28 21:36:12 -0700

    Update Example_RL
    added more notes

--- Assets/Scripts/Core/RL_Agent/Example_RL.cs
+++ Assets/Scripts/Core/RL_Agent/Example_RL.cs
@@ -1,5 +1,4 @@
-﻿using UnityEngine;
-using Unity.MLAgents;
+﻿using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 
@@ -21,19 +20,35 @@ namespace RL_Agent
         // read the state
         public override void CollectObservations(VectorSensor sensor)
         {
-            
+            // sensor.AddObservation is done here
+            // each on is Vector Observation Space Size ++ in inspector
         }
 
         // use this to mask actions
         public override void WriteDiscreteActionMask(IDiscreteActionMask actionMask)
         {
+            // this allows you to say for branch [x] disable choice y
+        }
+
+        // this is where you set up action spaces
+        public override void OnActionReceived(ActionBuffers actionBuffers)
+        {
+            // if using continuous actions - it is one branch with that amount of action space
             
+            // an action buffer has an amount of actions in it
+            
+            // not continuous is discrete
+            // - each branch can yield one action choice out of it
+            // - multiple branches should be used for unrelated action spaces
+            
+            // buffer[0] means branch 0, if it has 4 possible actions, give it value 5, 1 for the "empty" action
         }
 
         // user input influence
         public override void Heuristic(in ActionBuffers actionsOut)
         {
-            
+            // if you want to manually get the agent started
+            // or test that actions are working as designed?
         }
     }
 }
