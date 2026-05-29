dd1930a3608c7130ad6d79f4181b72665391dbd3
Nico Duvekot <nduvekot7@gmail.com>
2026-05-29 09:20:58 -0700

    Create UnitType variants
    SoldierUnit
    ArcherUnit
    HorsemanUnit

---
+++ Assets/Scripts/Core/Units/SoldierUnit.cs
@@ -0,0 +1,11 @@
+﻿namespace Units
+{
+    public class SoldierUnit : BaseUnit
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            UnitType = 0;
+        }
+    }
+}
